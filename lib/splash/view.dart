70afbaaa8f033bbff1079391499c1e7ba80a8799
menggod <menggod@126.com>
2020-12-22 13:57:58 +0800

    backup   fish redux

---
+++ lib/splash/view.dart
@@ -0,0 +1,11 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart';
+
+import 'action.dart';
+import 'state.dart';
+
+Widget buildView(SplashState state, Dispatch dispatch, ViewService viewService) {
+  return Scaffold(
+    body: Center(child: Text("haha")),
+  );
+}
