1106d911e00adbcab0b58576b1a8215616ece70e
menggod <menggod@126.com>
2020-12-24 18:28:48 +0800

    backup

--- lib/draw/day05/page.dart
+++ lib/draw/day05/page.dart
@@ -8,22 +8,21 @@ class TestPathPainter extends CustomPainter {
     final paint = Paint()
       ..color = Colors.pinkAccent
       ..strokeWidth = 4
-      ..strokeJoin=StrokeJoin.round
+      ..strokeJoin = StrokeJoin.round
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.fill;
     canvas.save();
     canvas.translate(size.width / 2, size.height / 2);
 
     var path = Path();
-
     path
       ..relativeMoveTo(0, 0)
       ..relativeLineTo(-30, 120)
       ..relativeLineTo(30, -30)
-      ..relativeLineTo( 30,30)
+      ..relativeLineTo(30, 30)
       ..close();
-    for(int i = 0; i < 8; i++){
-      canvas.drawPath(path.transform(Matrix4.rotationZ(i*pi/4).storage), paint);
+    for (int i = 0; i < 8; i++) {
+      canvas.drawPath(path.transform(Matrix4.rotationZ(i * pi / 4).storage), paint);
     }
     canvas.restore();
   }
@@ -34,13 +33,11 @@ class TestPathPainter extends CustomPainter {
   }
 }
 
-
 class HandleWidget extends StatefulWidget {
   final double size;
   final double handleRadius;
 
-  HandleWidget({Key key, this.size = 60.0, this.handleRadius = 30.0})
-      : super(key: key);
+  HandleWidget({Key key, this.size = 60.0, this.handleRadius = 30.0}) : super(key: key);
 
   @override
   _HandleWidgetState createState() => _HandleWidgetState();
@@ -49,9 +46,7 @@ class HandleWidget extends StatefulWidget {
 class _HandleWidgetState extends State<HandleWidget> {
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-        size: Size(widget.size, widget.size),
-        painter: _HandlePainter(handleR: widget.handleRadius));
+    return CustomPaint(size: Size(widget.size, widget.size), painter: _HandlePainter(handleR: widget.handleRadius));
   }
 }
 
@@ -78,6 +73,5 @@ class _HandlePainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(_HandlePainter oldDelegate) =>
-      oldDelegate.handleR != handleR;
+  bool shouldRepaint(_HandlePainter oldDelegate) => oldDelegate.handleR != handleR;
 }
