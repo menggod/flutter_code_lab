2d18246b363ce0768ebd81b1dd9393fa05b1008e
menggod <menggod@126.com>
2020-12-25 11:52:08 +0800

    backup

--- lib/splash/view.dart
+++ lib/splash/view.dart
@@ -1,13 +1,154 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_code_test/utils/color_utils.dart';
 import 'package:flutter_code_test/view/square_grid_view.dart';
 
-import 'action.dart';
 import 'state.dart';
 
 Widget buildView(SplashState state, Dispatch dispatch, ViewService viewService) {
-  return Scaffold(
-      body: Container(
-    child: SquareGridView(displayAxis: false),
-  ));
+  return Scaffold(body: _buildBody());
+}
+
+Container _buildBody() {
+  return Container(
+    child: Stack(
+      alignment: Alignment.topLeft,
+      children: [
+        SquareGridView(displayAxis: false),
+        Positioned(
+          left: 70,
+          top: 3,
+          child: Text(
+            'menggod',
+            style: TextStyle(color: ColorGenerator().getRandomColor()),
+          ),
+        ),
+        SafeArea(
+          child: _buildRealContainer(),
+        )
+      ],
+    ),
+  );
+}
+
+Widget _buildRealContainer() {
+  for (var i = 0; i < 20; i++) {
+    data.add(PoemItem(
+        image: AssetImage("assets/images/wy_200x300.jpg"),
+        title: "$i:以梦为马",
+        author: "海子",
+        summary: "我要做远方的忠诚的儿子，和物质的短暂情人，和所有以梦为马的诗人一样，我不得不和烈士和小丑走在同一道路上"));
+  }
+
+  return Container(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [Expanded(child: show)],
+    ),
+  );
+}
+
+var data = <PoemItem>[];
+
+var show = ListView.builder(
+    padding: EdgeInsets.all(8.0),
+    itemCount: data.length, //条目的个数
+    itemBuilder: (BuildContext context, int index) {
+      return PoemItemView(
+        //数据填充条目
+        data: data[index],
+        onItemClickListener: () {
+          //事件响应
+          print(index);
+        },
+      );
+    });
+
+typedef OnItemClickListener = void Function();
+
+class PoemItemView extends StatelessWidget {
+  final PoemItem data;
+  final OnItemClickListener onItemClickListener;
+
+  PoemItemView({Key key, this.data, this.onItemClickListener}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var headIcon = Container(
+        //左边头部
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              offset: Offset(0.0, 0.0),
+              blurRadius: 3.0,
+              spreadRadius: 0.0,
+            ),
+          ],
+        ),
+        width: 70,
+        height: 70,
+        child: Padding(
+          padding: EdgeInsets.all(3),
+          child: CircleAvatar(
+            backgroundImage: data.image,
+          ),
+        ));
+    var center = Column(
+      //中间介绍
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(data.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Padding(
+          padding: EdgeInsets.only(top: 8),
+          child: Text(
+            "作者:${data.author}",
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+        ),
+      ],
+    );
+    var summary = Text(
+      //尾部摘要
+      data.summary,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(color: Colors.grey, fontSize: 12),
+    );
+    var item = Row(
+      //条目拼合
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(width: 10),
+        headIcon,
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: center,
+        ),
+        Expanded(
+          child: summary,
+        ),
+        SizedBox(width: 10),
+      ],
+    );
+    var result = Card(
+      child: InkWell(
+        onTap: onItemClickListener,
+        child: item,
+      ),
+    );
+    return result;
+  }
+}
+
+class PoemItem {
+  ImageProvider image; //图片
+  var title; //标题
+  var author; //作者
+  var summary; //摘要
+  PoemItem({this.image, this.title, this.author, this.summary});
 }
--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 import 'dart:ui';
 
-Color randomRGB(){
+import 'package:flutter/material.dart';
+
+Color randomRGB() {
   Random random = new Random();
   int r = 30 + random.nextInt(200);
   int g = 30 + random.nextInt(200);
@@ -9,12 +11,63 @@ Color randomRGB(){
   return Color.fromARGB(255, r, g, b);
 }
 
-
-Color randomARGB(){
+Color randomARGB() {
   Random random = new Random();
   int r = 30 + random.nextInt(200);
   int g = 30 + random.nextInt(200);
   int b = 30 + random.nextInt(200);
   int a = 50 + random.nextInt(150);
   return Color.fromARGB(a, r, g, b);
+}
+
+class ColorGenerator {
+  static const defaultList = [
+    0xfff16364,
+    0xfff58559,
+    0xfff9a43e,
+    0xffe4c62e,
+    0xff67bf74,
+    0xff59a2be,
+    0xff2093cd,
+    0xffad62a7,
+    0xff805781
+  ];
+
+  static const materialList = [
+    0xffe57373,
+    0xfff06292,
+    0xffba68c8,
+    0xff9575cd,
+    0xff7986cb,
+    0xff64b5f6,
+    0xff4fc3f7,
+    0xff4dd0e1,
+    0xff4db6ac,
+    0xff81c784,
+    0xffaed581,
+    0xffff8a65,
+    0xffd4e157,
+    0xffffd54f,
+    0xffffb74d,
+    0xffa1887f,
+    0xff90a4ae
+  ];
+
+  List<int> mColors;
+  Random mRandom;
+
+  static ColorGenerator create(List<int> colorList) {
+    return ColorGenerator();
+  }
+
+  ColorGenerator({List<int> colorList = materialList}) {
+    mColors = colorList;
+    mRandom = Random(DateTime.now().millisecondsSinceEpoch);
+  }
+
+  Color getColor(Object key) => Color(mColors[key.hashCode.abs() % mColors.length]);
+
+  Color getRandomColor() {
+    return Color(mColors[mRandom.nextInt(mColors.length)]);
+  }
 }
