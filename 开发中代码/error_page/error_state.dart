7ec4637f9cdd423a3d96e67efecc150311b5d6ac
menggod <menggod@126.com>
2023-09-17 11:47:03 +0800

    feat: 开发过程中的代码

--- 开发中代码/error_page/error_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mpa/models/response_bean.dart';
-
-class PageState {
-  bool isShowErrorPage = false;
-  bool? isShowLoading = false;
-  BaseResponse? baseResponse;
-  int? pageInitMs;
-}
